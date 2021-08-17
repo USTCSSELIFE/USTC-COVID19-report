4faa116e0681232a6a03ea39cf5145e979c05d64
Kasper4649 <imzxz123456@gmail.com>
2021-08-17 14:30:00 +0800

    fix: fix format time bugs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"os"
 	"time"
@@ -15,6 +16,7 @@ const (
 )
 
 var logger zerolog.Logger
+var haveReportError = errors.New("have reported")
 
 func main() {
 	file, err := os.OpenFile("./log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -25,23 +27,28 @@ func main() {
 	logger = zerolog.New(file).With().Timestamp().Caller().Logger()
 
 	for i := 0; i < 5; i++ {
-		reportTime, ok := report()
-		if !ok {
-			logger.Info().Msgf("have reported at %v\n", reportTime)
+		reportTime, err := report()
+		if err != nil {
+			if errors.Is(err, haveReportError) {
+				logger.Warn().Time("lastReportTime", reportTime).Msg(err.Error())
+				return
+			}
+			logger.Error().Msgf("fail to report: %v", err)
 			return
 		}
 		if isTimeValid(reportTime) {
-			logger.Info().Msgf("succeed to report at %v\n", reportTime)
+			logger.Info().Msgf("succeed to report at %v", reportTime)
 			return
 		}
 		// if one doesn't report will automatically receive a mail by USTC,
 		// so not necessarily to tell the failure
-		logger.Error().Msgf("fail to report: %v\n", reportTime)
-		logger.Info().Msgf("retry to report %d times\n", i+1)
+		logger.Error().Time("lastReportTime", reportTime).
+			Int("retry times", i+1).
+			Msg("fail to report")
 	}
 }
 
-func report() (time.Time, bool) {
+func report() (time.Time, error) {
 	u := launcher.New().NoSandbox(true).MustLaunch()
 	page := rod.New().ControlURL(u).MustConnect().MustIncognito().MustPage(loginUrl).MustWindowFullscreen()
 	page.MustElement("#username").MustInput(username)
@@ -50,11 +57,15 @@ func report() (time.Time, bool) {
 	timeText := page.MustWaitLoad().MustElementX("//div[@id='daliy-report']//span//strong").MustText()
 
 	// if one has reported
-	if haveReported(formatTime(timeText)) {
-		return formatTime(timeText), false
+	t, err := formatTime(timeText)
+	if err != nil {
+		return time.Time{}, err
+	}
+	if haveReported(t) {
+		return time.Time{}, haveReportError
 	}
 
 	page.MustElement("#report-submit-btn").MustClick()
 	timeText = page.MustWaitLoad().MustElementX("//div[@id='daliy-report']//span//strong").MustText()
-	return formatTime(timeText), true
+	return t, nil
 }
--- time.go
+++ time.go
@@ -2,24 +2,15 @@ package main
 
 import (
 	"github.com/golang-module/carbon"
-	"regexp"
 	"time"
 )
 
-func formatTime(timeText string) time.Time {
-	regx := regexp.MustCompile("2021-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}")
-	reportTimeString := regx.FindString(timeText)
-
-	location, err := time.LoadLocation("Asia/Shanghai")
-	if err != nil {
-		logger.Err(err)
+func formatTime(timeText string) (time.Time, error) {
+	c := carbon.ParseByFormat(timeText, "*上次上报时间：Y-m-d H:i:s，请每日按时打卡", carbon.Shanghai)
+	if c.Error != nil {
+		return time.Time{}, c.Error
 	}
-	reportTime, err := time.ParseInLocation("2006-01-02 15:04:05", reportTimeString, location)
-	if err != nil {
-		logger.Err(err)
-	}
-	return reportTime
-
+	return c.Carbon2Time(), nil
 }
 
 func isTimeValid(reportTime time.Time) bool {
@@ -30,7 +21,7 @@ func haveReported(reportTime time.Time) bool {
 	cReportTime := carbon.Time2Carbon(reportTime)
 	cNow := carbon.Time2Carbon(time.Now())
 
-	if time.Since(reportTime) <= 0 {
+	if cReportTime.IsFuture() {
 		return false
 	}
 	if cReportTime.BetweenIncludedBoth(cNow.StartOfDay(), cNow) {
